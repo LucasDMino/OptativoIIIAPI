f05e4f5673e63a4490dd55d87cdd569c7c0fcf24
Lucas Miño <94700933+LucasDMino@users.noreply.github.com>
2023-10-26 01:34:36 -0300

    Create Ciudad.cs

---
+++ Ciudad.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASPWEBAPI2.Models
+{
+    public class Ciudad
+    {
+        public int idCiudad { get; set; }
+        public string CiudadNombre { get; set; }
+        public string Departamento { get; set; }
+        public int PostalCode { get; set; }
+
+    }
+}
+
+//    idCiudad 
+//CiudadNombre 
+//Departamento 
+//PostalCode 
